8e39d5d210cb7460572b2a6cb8e242d3e5269ca9
Ni30kr91 <nitish.kumar8092@gmail.com>
2022-06-08 15:29:10 +0530

    adding of 3

---
+++ T3.go
@@ -0,0 +1,16 @@
+package main
+ 
+import "fmt"
+
+func main () {
+	fmt.Println("3 x 1 = 3")
+	fmt.Println("3 x 2 = 6")
+	fmt.Println("3 x 3 = 9")
+	fmt.Println("3 x 4 = 12")
+	fmt.Println("3 x 5 = 15")
+	fmt.Println("3 x 6 = 18")
+	fmt.Println("3 x 7 = 21")
+	fmt.Println("3 x 8 = 24")
+	fmt.Println("3 x 9 = 27")
+	fmt.Println("3 x 10 = 30")
+}
